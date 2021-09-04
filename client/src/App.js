c1c814c058d3c47c48779bd31c39b050aeddfa50
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-09-04 16:57:52 -0700

    re-initalize with event bus

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import PostCreate from './PostCreate';
-import PostList from './PostList';
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-const App = () => (
+const App = () => {
+  return (
     <div className="container">
-        <h1>Create Post</h1>
-        <PostCreate />
-        <h1>Post List</h1>
-        <PostList />
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
-);
-
+  );
+};
 export default App;
--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect }from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const CommentList = ({ postId }) => {
-    const [comments, setComments] = useState([]);
-    
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`); 
-        setComments(res.data);
-    };
+  const [comments, setComments] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    });
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
 
-    const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>;
-    });
+    setComments(res.data);
+  };
 
-    return <ul>{renderedComments}</ul>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
 
 export default CommentList;
