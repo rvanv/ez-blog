d4375b5e464b0465729e4d30294b5f2dccb6bd0f
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-09-03 17:36:39 -0700

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => (
-    <div>Blog App</div>
+    <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate />
+        <h1>Post List</h1>
+        <PostList />
+    </div>
 );
 
 export default App;
