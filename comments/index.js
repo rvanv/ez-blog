6545d1fbdff1621e80b014095f1ed80bf10a2344
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-09-04 21:55:14 -0700

    wip-broken

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -30,6 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending'
     },
   }).catch((err) => {
     console.log(err.message);
@@ -38,11 +39,36 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event Received", req.body.type);
 
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        postId,
+        status
+      }
+
+    }).catch((err) => {
+      console.log(err.message);
+    });
+
   res.send({});
-});
+}});
 
 app.listen(4001, () => {
   console.log("Listening on 4001");
--- query/index.js
+++ query/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts);
 });
 
 app.post('/event', (req, res) => {
@@ -22,11 +22,23 @@ app.post('/event', (req, res) => {
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
 
-        post.comments.push({ id, content })
+        post.comments.push({ id, content, status })
+    }
+
+    if (type === 'CommentUpdated') {
+        const { id, postId, content, status } = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status;
+        comment.content = content;
     }
 
     res.send({}); 
